d95fc9fe378fe7c57ba32bc4178c2cfcbf8b0784
alviranarshidani <alviranarshidani7@gmail.com>
2019-05-30 00:48:54 -0400

    comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
 
 db.serialize(function(){
+    //1.Create an in memory database with a ‘Classroom’ and ‘Department’ table containing the above relations.
     db.run("CREATE TABLE Classroom (Building TEXT, Room_number NUMBER, Capacity NUMBER)");
     db.run("CREATE TABLE Department (Dept_name TEXT, Building TEXT, Budget NUMBER)");
 
@@ -20,17 +21,21 @@ db.serialize(function(){
     db.run("INSERT INTO Department VALUES('Music', 'Packard',80000)");
     db.run("INSERT INTO Department VALUES('Physics', 'Watson',70000)");
 
+    //2.Print the room number and building name for those rooms whose capacity is greater than 50.
     db.each("SELECT Room_number, Building FROM Classroom WHERE Capacity>50",
     function(err,row){
             //console.log(err);
             console.log(row);
     });
 
+    //3.Print the names of those departments whose budgets are greater than $85,000.
     db.each("SELECT Dept_name FROM Department WHERE Budget>85000",
     function(err,row){
             //console.log(err);
             console.log(row);
     });
+
+    //4.For each department, print the total capacity available.
 
     
 
