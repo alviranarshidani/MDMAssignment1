d9fa4f1da8de6e4b1e2b19304778e473da587bee
alviranarshidani <alviranarshidani7@gmail.com>
2019-06-05 22:42:49 -0400

    formatting

--- index.js
+++ index.js
@@ -22,24 +22,59 @@ db.serialize(function(){
     db.run("INSERT INTO Department VALUES('Physics', 'Watson',70000)");
 
     //2.Print the room number and building name for those rooms whose capacity is greater than 50.
+    console.log("Following are Room No. & Building name whose capacity is greater than 50:");
     db.each("SELECT Room_number, Building FROM Classroom WHERE Capacity>50",
     function(err,row){
-            //console.log(err);
-            console.log(row);
-    });
+                console.log("Romm No."+row.Room_number +' in '+ row.Building +' building')
+        }); 
 
     //3.Print the names of those departments whose budgets are greater than $85,000.
-    db.each("SELECT Dept_name FROM Department WHERE Budget>85000",
-    function(err,row){
-            //console.log(err);
-            console.log(row);
-    });
+    let Departments =new Array();
+    db.each(
+        "SELECT Dept_name FROM Department WHERE Budget>85000",
+        function(err,row){
+                Departments.push(row.Dept_name)
+        },
+        function(err,count){
+                let resString = "";
+                for(let i = 0; i != Departments.length; i++){
+                        if (i != count - 1) {
+                                resString += Departments[i] + ", "
+                        }
+                        else
+                        resString += Departments[i];
+                }
+                console.log('Departments with budget greater than 85000 => '+resString);
+        }
+    );
 
-    //4.For each department, print the total capacity available.
+    //4.For each department, print the total capacity available
+        let results =new Array();
+        db.each(
+                "SELECT Department.Dept_name as Dept_name,sum(Classroom.Capacity) as Capacity FROM Department,Classroom WHERE Department.Building like Classroom.Building GROUP BY Department.Dept_name",
+                function(err,row){
+                        results.push(row.Dept_name,': '+row.Capacity+'      ')
+                },
+                function(err,count){
+                        let resultString = "";
+                        for(let i = 0; i != results.length; i++){
+                        if (i != count - 1) {
+                                resultString += results[i]
+                        }
+                        else
+                                resultString += results[i]
+        
+                        }
+                console.log('Departments and its capacity => '+ resultString);
+                }
+        );
 
-    
+/* ****OUTPUT***
+Following are Room No. & Building name whose capacity is greater than 50:
+Romm No.101 in Packard building
+Romm No.3128 in Tylor building
+Departments with budget greater than 85000 => Biology, Comp. Sci., Finance
+Departments and its capacity => Biology: 80      Finance: 10      History: 10      Music: 500      Physics: 80
+****OUTPUT*** */
 
-    
-
-    
 });
